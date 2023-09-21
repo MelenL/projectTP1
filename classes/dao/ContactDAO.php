8bc5b02e5d9e3cebc365b56468a9b02d94de2ba2
Melen Laclais <melen.laclais@etudiant.univ-rennes1.fr>
2023-09-21 17:00:17 +0200

    ContactDAO finit

--- classes/dao/ContactDAO.php
+++ classes/dao/ContactDAO.php
@@ -8,27 +8,82 @@ class ContactDAO {
 
     // Méthode pour insérer un nouveau contact dans la base de données
     public function create(ContactModel $contact) {
+        //On importe notre variable global de connexion.
+        global $pdo; 
         
+        $sql = "INSERT INTO contacts (nom,prenom,email,telephone) VALUES (:nom, :prenom, :email, :telephone)"; //On enregistre dans une variable notre requête SQL
+
+        $stmt = $pdo->prepare($sql); //On prépare notre requête.
+
+        //On remplace mes VALUES par nos paramètres.
+        $stmt->bindParam(':nom', $nom);
+        $stmt->bindParam(':prenom', $prenom);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':telephone', $telephone);
+
+        $stmt->execute(); //On execute notre requête
     }
 
     // Méthode pour récupérer un contact par son ID
     public function getById($id) {
-       
+        //On importe notre variable global de connexion.
+        global $pdo; 
+        
+        $sql = "SELECT * FROM contacts WHERE id = :id"; //On enregistre dans une variable notre requête SQL
+
+        $stmt = $pdo->prepare($sql); //On prépare notre requête.
+
+        //On remplace mes VALUES par nos paramètres.
+        $stmt->bindParam(':id', $id);
+
+        $stmt->execute(); //On execute notre requête
     }
 
     // Méthode pour récupérer la liste de tous les contacts
     public function getAll() {
-       
+        //On importe notre variable global de connexion.
+        global $pdo; 
+        
+        $sql = "SELECT * FROM contacts"; //On enregistre dans une variable notre requête SQL
+
+        $stmt = $pdo->prepare($sql); //On prépare notre requête.
+        
+        $stmt->execute(); //On execute notre requête
     }
 
     // Méthode pour mettre à jour un contact
     public function update(ContactModel $contact) {
-       
+        //On importe notre variable global de connexion.
+        global $pdo; 
+        
+        $sql = "UPDATE contacts SET nom = :nom, prenom = :prenom, email = :email, telephone = :telephone WHERE id = :id"; //On enregistre dans une variable notre requête SQL
+
+        $stmt = $pdo->prepare($sql); //On prépare notre requête.
+
+        //On remplace mes VALUES par nos paramètres.
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':nom', $nom);
+        $stmt->bindParam(':prenom', $prenom);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':telephone', $telephone);
+
+        $stmt->execute(); //On execute notre requête
     }
 
     // Méthode pour supprimer un contact par son ID
     public function deleteById($id) {
-       
+        //On importe notre variable global de connexion.
+        global $pdo; 
+        
+        $sql = "DELETE FROM contacts WHERE id = :id"; //On enregistre dans une variable notre requête SQL
+
+        $stmt = $pdo->prepare($sql); //On prépare notre requête.
+
+        //On remplace mes VALUES par nos paramètres.
+
+        $stmt->bindParam(':id', $id);
+
+        $stmt->execute(); //On execute notre requête
     }
 }
 ?>
--- classes/models/ContactModel.php
+++ classes/models/ContactModel.php
@@ -6,7 +6,56 @@ class ContactModel {
     private $email;
     private $telephone;
     
-    //Rajouter le constructeur, les getters et setters
-    // Vous pouvez ajouter des méthodes supplémentaires ici pour manipuler les données du contact
+    public function __construct($id, $nom, $prenom, $email, $telephone) { //Création d'un contact
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->email = $email;
+        $this->telephone = $telephone;
+    }
+
+    //Getters pour chaque variables.
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNom() {
+        return $this->nom;
+    }
+
+    public function getPrenom() {
+        return $this->prenom;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getTelephone() {
+        return $this->telephone;
+    }
+
+    //Setters pour chaque variable.
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setNom($nom) {
+        $this->nom = $nom;
+    }
+
+    public function setPrenom($prenom) {
+        $this->prenom = $prenom;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function setTelephone($telephone) {
+        $this->telephone = $telephone;
+    }
+
 }
 ?>
